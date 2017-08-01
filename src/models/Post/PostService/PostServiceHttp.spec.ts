937d23556981a5b1c83b9e63bc032753a69a4d38
Pavel Gafarov <gothic27prince@gmail.com>
2017-08-01 15:29:18 +0300

    PostService:Conference

--- src/models/Post/PostService/PostServiceHttp.spec.ts
+++ src/models/Post/PostService/PostServiceHttp.spec.ts
@@ -5,19 +5,37 @@ describe('PostServiceHttp', () => {
   const storage = new PostStorageEntities();
   const factory = new PostServiceHttpFactory();
   storage.reset();
-  /*
+
   it ('Should return conferences of user with id 100', (done) => {
     const service = factory.get(PostServiceHttpFactory.CATEGORY_CONFERENCE, 100, storage);
     if (service instanceof PostServiceHttp) {
       service.fetch(() => {
+        console.log('OK');
         expect(service.all().length).toBe(1);
-        expect(service.all()[0].getId()).toBe(100);
-        expect(service.all()[0]).toBe(storage.get(100));
+        expect(service.all()[0].getId()).toBe(100000);
+        // todo: Get users
+        // expect(service.all()[0].getUserManager().get('[CONFERENCE]').getUsers().length).toBe(2);
+        expect(service.all()[0].getPostManager().get(PostServiceHttpFactory.CATEGORY_MESSAGE).all().length).toBe(4);
+        // todo: Check each item
         done();
       });
     }
   });
-  */
+  it ('Should return conferences of user with id 102', (done) => {
+    const service = factory.get(PostServiceHttpFactory.CATEGORY_CONFERENCE, 102, storage);
+    if (service instanceof PostServiceHttp) {
+      service.fetch(() => {
+        console.log('OK2');
+        expect(service.all().length).toBe(2);
+        // todo: Get users
+        // expect(service.all()[0].getUserManager().get('[CONFERENCE]').getUsers().length).toBe(2);
+        expect(service.all()[0].getPostManager().get(PostServiceHttpFactory.CATEGORY_MESSAGE).all().length).toBe(4);
+        expect(service.all()[1].getPostManager().get(PostServiceHttpFactory.CATEGORY_MESSAGE).all().length).toBe(3);
+        // todo: Check each item
+        done();
+      });
+    }
+  });
   /*
   it ('Should return message of user with id 100', (done) => {
     const service = factory.get(PostServiceHttpFactory.CATEGORY_POSTS, 100, storage);
@@ -31,5 +49,4 @@ describe('PostServiceHttp', () => {
     }
   });
   */
-  
 });
--- src/models/Post/PostService/PostServiceHttp.ts
+++ src/models/Post/PostService/PostServiceHttp.ts
@@ -7,6 +7,7 @@ import {FileManager} from '../../File/FileManager/FileManager';
 import {PostManager} from '../PostManager/PostManager';
 import {UserManager} from '../../User/UserManager/UserManager';
 import {PostServiceHttpFactory} from 'models/Post/Factories/PostServiceHttpFactory';
+import {MessagePostFetchModel} from '../FetchModel/MessagePostFetchModel';
 
 export class PostServiceHttp extends PostService {
   protected fetchModel: FetchModelInterface;
@@ -21,13 +22,12 @@ export class PostServiceHttp extends PostService {
   public fetch (succesFunc) {
     switch (this.getName()) {
       case PostServiceHttpFactory.CATEGORY_CONFERENCE:
-        this.fetchModel.all((id) => {
-          console.log(id);
-        }, succesFunc);
+        this.findAll(succesFunc);
         break;
       case PostServiceHttpFactory.CATEGORY_COMMENTS:
         break;
       case PostServiceHttpFactory.CATEGORY_POSTS:
+        this.findAll(succesFunc);
         break;
       case PostServiceHttpFactory.CATEGORY_MESSAGE:
         break;
@@ -35,14 +35,45 @@ export class PostServiceHttp extends PostService {
         break;
     }
   }
+  protected findAll (succesFunc) {
+    const ips = [];
+    this.getFetchModel().all((id) => {
+      ips.push(id);
+    }, () => {
+      this.findAllByIds(ips, this.storage, succesFunc)
+    });
+  }
+  protected findAllByIds(ids: Number[], storage: PostStorageEntities, succesFunc) {
+    let i = 0;
+    ids.map((id: Number) => {
+      this.getMessagePostById(id, storage, () => {
+        i++;
+        if (i === (ids.length)) {
+          succesFunc();
+        }
+      });
+    });
+  }
   // todo: FETCH
-  protected findById (id: Number, storage: PostStorageEntities, succesFunc) {
+  protected getMessagePostById (id: Number, storage: PostStorageEntities, succesFunc) {
     const factory = new PostServiceHttpFactory();
     const service = factory.get(PostServiceHttpFactory.CATEGORY_MESSAGE, id, storage);
     service.getFetchModel().get((data) => {
-      this.storage.add(
-        data.id, new PostEntity(
-          new Date(), data.id, new InfoManager(), new FileManager(), new PostManager(), new UserManager()
+      const posts = data.posts.map((itemid) => {
+        // todo: connect
+        return this.storage.add(
+          itemid, new PostEntity(
+            new Date(), itemid, new InfoManager(), new FileManager(), new PostManager(), new UserManager()
+          )
+        );
+      });
+      const postManager = new PostManager();
+      postManager.add(new PostService(PostServiceHttpFactory.CATEGORY_MESSAGE, data.posts, storage))
+      this.add(
+        this.storage.add(
+          data.id, new PostEntity(
+            new Date(), data.id, new InfoManager(), new FileManager(), postManager, new UserManager()
+          )
         )
       )
     }, succesFunc);
